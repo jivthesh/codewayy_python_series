7c30fa0c5d6bf12c26fe0b7fdecd8c24888b996b
Susmita Das <62754933+susmita4761@users.noreply.github.com>
2020-06-25 08:43:42 -0700

    Delete Question2_func.py

--- Python_Task 3/Question2_func.py
+++
@@ -1,9 +0,0 @@
-#defining string 1
-string1=input("Enter First String:")
-#defining string 2
-string2=input("Enter Second String:")
-#concatenating two strings
-con_string=string1+string2
-#display the length of the string
-len_string=len(con_string)
-print("Length of String:",len_string)
